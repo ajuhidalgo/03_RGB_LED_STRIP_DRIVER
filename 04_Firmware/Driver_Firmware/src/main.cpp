cf13e353148a7e29f620a84aa146fc0ad17760bf
ajuhidalgo <ajuhidalgo@gmail.com>
2024-07-02 19:14:22 +0200

    Firmware update for esp32 support

--- 04_Firmware/Driver_Firmware/src/main.cpp
+++ 04_Firmware/Driver_Firmware/src/main.cpp
@@ -1,28 +1,33 @@
+
+// RGB LED STRIP DRIVER FIRMWARE.
+
+// Libraries.
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <SinricPro.h>
 #include <SinricProLight.h>
 #include <map>
 
-#define WIFI_SSID  "Livebox6-9460"
-#define WIFI_PASS  "QgQw9LASYq66"
-#define APP_KEY    "f861277b-595d-4605-b841-663e9a68bba0"
-#define APP_SECRET "57ffd202-fb21-48ba-922e-c63584829f6d-230ec330-54d0-4652-8f70-71883d89977b"
-#define LIGHT_ID   "6618011c7c9e6c6fe868df8a"
-#define BAUD_RATE  115200
+// Global constants.
+const char wifi_ssid[]  = "YOUR-WIFI-SSID";
+const char wifi_pass[]  = "YOUR-WIFI-PASS";
+const char app_key[]    = "YOUR-APPKEY";
+const char app_secret[] = "YOUR-APPSECRET";
+const char light_id[]   = "YOUR-DEVICEID";
+const int baud_rate     = 115200;           // Baudrate to serial log
+const int blue_pin      = 20;               // PIN for BLUE Mosfet
+const int red_pin       = 22;               // PIN for RED Mosfet
+const int green_pin     = 21;               // PIN for GREEN Mosfet
+SinricProLight& myLight = SinricPro[light_id];  // SinricProLight device.
 
-#define BLUE_PIN  13  // PIN for BLUE Mosfet
-#define RED_PIN   16  // PIN for RED Mosfet
-#define GREEN_PIN 12  // PIN for GREEN Mosfet
-
+// Global variables.
 struct Color {
     uint8_t r;
     uint8_t g;
     uint8_t b;
 };
 
-// Colortemperature lookup table
-using ColorTemperatures = std::map<uint16_t, Color>;
+using ColorTemperatures = std::map<uint16_t, Color>; // Colortemperature lookup table.
 ColorTemperatures colorTemperatures{
     //   {Temperature value, {color r, g, b}}
     {2000, {255, 138, 18}},
@@ -45,116 +50,126 @@ struct DeviceState {                                   // Stores current device 
     int   brightness       = 100;                      // brightness is set to 100
 } device_state;
 
-SinricProLight& myLight = SinricPro[LIGHT_ID];  // SinricProLight device
 
+// Functions.
 void setStripe() {
-    int rValue = map(device_state.color.r * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate red value and map between 0 and 1023 for analogWrite
-    int gValue = map(device_state.color.g * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate green value and map between 0 and 1023 for analogWrite
-    int bValue = map(device_state.color.b * device_state.brightness, 0, 255 * 100, 0, 1023);  // calculate blue value and map between 0 and 1023 for analogWrite
+    int rValue = map(device_state.color.r * device_state.brightness, 0, 255 * 100, 0, 1023);  // Calculate red value and map between 0 and 1023 for analogWrite.
+    int gValue = map(device_state.color.g * device_state.brightness, 0, 255 * 100, 0, 1023);  // Calculate green value and map between 0 and 1023 for analogWrite.
+    int bValue = map(device_state.color.b * device_state.brightness, 0, 255 * 100, 0, 1023);  // Calculate blue value and map between 0 and 1023 for analogWrite.
 
-    if (device_state.powerState == false) {  // turn off?
-        digitalWrite(RED_PIN, LOW);          // set
-        digitalWrite(GREEN_PIN, LOW);        // mosfets
-        digitalWrite(BLUE_PIN, LOW);         // low
-    } else {
-        analogWrite(RED_PIN, rValue);    // write red value to pin
-        analogWrite(GREEN_PIN, gValue);  // write green value to pin
-        analogWrite(BLUE_PIN, bValue);   // write blue value to pin
+    if (device_state.powerState == false) {  // Turn off?.
+        digitalWrite(red_pin, LOW);          // Set.
+        digitalWrite(green_pin, LOW);        // Mosfets.
+        digitalWrite(blue_pin, LOW);         // Low.
+    } 
+    else {
+        analogWrite(red_pin, rValue);       // Write red value to pin.
+        analogWrite(green_pin, gValue);     // Write green value to pin.
+        analogWrite(blue_pin, bValue);      // Write blue value to pin.
     }
 }
 
 bool onPowerState(const String& deviceId, bool& state) {
-    device_state.powerState = state;  // store the new power state
-    setStripe();                      // update the mosfets
+    device_state.powerState = state;  // Store the new power state.
+    setStripe();                      // Update the mosfets.
     return true;
 }
 
 bool onBrightness(const String& deviceId, int& brightness) {
-    device_state.brightness = brightness;  // store new brightness level
-    setStripe();                           // update the mosfets
+    device_state.brightness = brightness;  // Store new brightness level.
+    setStripe();                           // Spdate the mosfets.
     return true;
 }
 
 bool onAdjustBrightness(const String& deviceId, int& brightnessDelta) {
-    device_state.brightness += brightnessDelta;  // calculate and store new absolute brightness
-    brightnessDelta = device_state.brightness;   // return absolute brightness
-    setStripe();                                 // update the mosfets
+    device_state.brightness += brightnessDelta;  // Calculate and store new absolute brightness.
+    brightnessDelta = device_state.brightness;   // Return absolute brightness.
+    setStripe();                                 // Update the mosfets.
     return true;
 }
 
 bool onColor(const String& deviceId, byte& r, byte& g, byte& b) {
-    device_state.color.r = r;  // store new red value
-    device_state.color.g = g;  // store new green value
-    device_state.color.b = b;  // store new blue value
-    setStripe();               // update the mosfets
+    device_state.color.r = r;  // Store new red value.
+    device_state.color.g = g;  // Store new green value.
+    device_state.color.b = b;  // Store new blue value.
+    setStripe();               // Update the mosfets.
     return true;
 }
 
 bool onColorTemperature(const String& deviceId, int& colorTemperature) {
-    device_state.color            = colorTemperatures[colorTemperature];  // set rgb values from corresponding colortemperauture
-    device_state.colorTemperature = colorTemperature;                     // store the current color temperature
-    setStripe();                                                          // update the mosfets
+    device_state.color            = colorTemperatures[colorTemperature];  // Set rgb values from corresponding colortemperauture.
+    device_state.colorTemperature = colorTemperature;                     // Store the current color temperature.
+    setStripe();                                                          // Update the mosfets.
     return true;
 }
 
 bool onIncreaseColorTemperature(const String& devceId, int& colorTemperature) {
-    auto current = colorTemperatures.find(device_state.colorTemperature);  // get current entry from colorTemperature map
-    auto next    = std::next(current);                                     // get next element
-    if (next == colorTemperatures.end()) next = current;                   // prevent past last element
-    device_state.color            = next->second;                          // set color
-    device_state.colorTemperature = next->first;                           // set colorTemperature
-    colorTemperature              = device_state.colorTemperature;         // return new colorTemperature
+    auto current = colorTemperatures.find(device_state.colorTemperature);  // Get current entry from colorTemperature map.
+    auto next    = std::next(current);                                     // Get next element.
+    if (next == colorTemperatures.end()) next = current;                   // Prevent past last element.
+    device_state.color            = next->second;                          // Set color.
+    device_state.colorTemperature = next->first;                           // Set colorTemperature.
+    colorTemperature              = device_state.colorTemperature;         // Return new colorTemperature.
     setStripe();
     return true;
 }
 
 bool onDecreaseColorTemperature(const String& devceId, int& colorTemperature) {
-    auto current = colorTemperatures.find(device_state.colorTemperature);  // get current entry from colorTemperature map
-    auto next    = std::prev(current);                                     // get previous element
-    if (next == colorTemperatures.end()) next = current;                   // prevent before first element
-    device_state.color            = next->second;                          // set color
-    device_state.colorTemperature = next->first;                           // set colorTemperature
-    colorTemperature              = device_state.colorTemperature;         // return new colorTemperature
+    auto current = colorTemperatures.find(device_state.colorTemperature);  // Get current entry from colorTemperature map.
+    auto next    = std::prev(current);                                     // Get previous element.
+    if (next == colorTemperatures.end()) next = current;                   // Prevent before first element.
+    device_state.color            = next->second;                          // Set color.
+    device_state.colorTemperature = next->first;                           // Set colorTemperature.
+    colorTemperature              = device_state.colorTemperature;         // Return new colorTemperature.
     setStripe();
     return true;
 }
 
 void setupWiFi() {
-    Serial.printf("WiFi: connecting");
-    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    Serial.printf("WiFi: connecting ...");
+    WiFi.setSleep(false); 
     WiFi.setAutoReconnect(true);
-    WiFi.begin(WIFI_SSID, WIFI_PASS); 
-    
+    WiFi.begin(wifi_ssid,wifi_pass); 
     while (WiFi.status() != WL_CONNECTED) {
         Serial.printf(".");
         delay(250);
     }
-
     Serial.printf("connected\r\nIP is %s\r\n", WiFi.localIP().toString().c_str());
 }
 
 void setupSinricPro() {
-    myLight.onPowerState(onPowerState);                              // assign onPowerState callback
-    myLight.onBrightness(onBrightness);                              // assign onBrightness callback
-    myLight.onAdjustBrightness(onAdjustBrightness);                  // assign onAdjustBrightness callback
-    myLight.onColor(onColor);                                        // assign onColor callback
-    myLight.onColorTemperature(onColorTemperature);                  // assign onColorTemperature callback
-    myLight.onDecreaseColorTemperature(onDecreaseColorTemperature);  // assign onDecreaseColorTemperature callback
-    myLight.onIncreaseColorTemperature(onIncreaseColorTemperature);  // assign onIncreaseColorTemperature callback
-    SinricPro.begin(APP_KEY, APP_SECRET);  // start SinricPro
+    myLight.onPowerState(onPowerState);                              // Assign onPowerState callback.
+    myLight.onBrightness(onBrightness);                              // Assign onBrightness callback.
+    myLight.onAdjustBrightness(onAdjustBrightness);                  // Assign onAdjustBrightness callback.
+    myLight.onColor(onColor);                                        // Assign onColor callback.
+    myLight.onColorTemperature(onColorTemperature);                  // Assign onColorTemperature callback.
+    myLight.onDecreaseColorTemperature(onDecreaseColorTemperature);  // Assign onDecreaseColorTemperature callback.
+    myLight.onIncreaseColorTemperature(onIncreaseColorTemperature);  // Assign onIncreaseColorTemperature callback.
+
+    SinricPro.begin(app_key,app_secret);  // start SinricPro
 }
 
+
+// Configuration.
 void setup() {
-    Serial.begin(BAUD_RATE);  // setup serial
 
-    pinMode(RED_PIN, OUTPUT);    // set red-mosfet pin as output
-    pinMode(GREEN_PIN, OUTPUT);  // set green-mosfet pin as output
-    pinMode(BLUE_PIN, OUTPUT);   // set blue-mosfet pin as output
+    // UART configuration.
+    Serial.begin(baud_rate);
+    Serial.println("Serial communication started ...");
+    
+    // Pin configuration.
+    pinMode(red_pin,OUTPUT);    // Set red-mosfet pin as output.
+    pinMode(green_pin,OUTPUT);  // Set green-mosfet pin as output.
+    pinMode(blue_pin,OUTPUT);   // Set blue-mosfet pin as output.
 
-    setupWiFi();       // connect wifi
-    setupSinricPro();  // setup SinricPro
+    // Wi-Fi connection.
+    setupWiFi();
+
+    // Sinric Pro configuration.
+    setupSinricPro();
 }
 
+// Loop.
 void loop() {
-    SinricPro.handle();  // handle SinricPro communication
+    SinricPro.handle();
 }
